1d8f427c3d01d447c9a9677e5101ddd5785441f9
Djonatan Weber <djonatan11021996@gmail.com>
2024-10-07 19:32:32 -0300

    feat: using history.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+console.log("Hello DOM");
+
+// history.back() // volta a página anterior
+// history.forward() // Avança para a próxima página
+// history.go(-2) // navega para trás duas páginas
+
+
