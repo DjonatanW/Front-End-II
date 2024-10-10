d48c5e1400b5ac08f8d2149800a16fa9a058f05d
Djonatan Weber <djonatan11021996@gmail.com>
2024-10-09 21:56:30 -0300

    feat:class 2

---
+++ class 2/scripts2.js
@@ -0,0 +1,41 @@
+// const button = document.querySelector('button')
+
+// function clickEvent(event) {
+//   // console.log('Evento de clique ocorreu!');
+//   // console.log(event);
+//   // console.log(event.type);
+//   // console.log(event.clientX, event.clientY);
+//   console.log(this);
+//   this.textContent = 'O texto mudou!'
+  
+  
+  
+// }
+
+// button.addEventListener('click', clickEvent)
+
+
+
+// const element = document.querySelector('.element')
+
+// function highlightedElement() {
+//   element.classList.add('highlighted')
+// }
+
+// function unhighlightedElement() {
+//   element.classList.remove('highlighted')
+// }
+// // quando passa o mouse
+// element.addEventListener('mouseover', highlightedElement)
+// element.addEventListener('mouseout', unhighlightedElement)
+
+// const input = document.getElementById('input')
+// const feedback = document.getElementById('feedback')
+
+// input.addEventListener('keydown', (event) => {
+//   feedback.textContent = `Tecla pressionada: ${event.key}`
+// })
+
+// input.addEventListener('keyup', () =>{
+//   feedback.textContent = 'Tecla liberada'
+// })
