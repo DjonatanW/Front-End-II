dabe33b61c8241be0103be88a7ed9fc22b140bb9
Djonatan Weber <djonatan11021996@gmail.com>
2024-10-14 19:52:30 -0300

    feat: class 3 users and notes.

---
+++ class-3/users/scripts.js
@@ -0,0 +1,28 @@
+const userContainer = document.querySelector('.user-list')
+const loadingIndicator = document.querySelector('.loading')
+
+const userCardTemplate = document.getElementById('user-card-template').content
+
+async function fetchUsers() {
+  loadingIndicator.style.display = 'block'
+  userContainer.innerHTML = ''
+
+  try {
+    const response = await axios.get('https://regres.in/api/users')
+    const users = response.data
+    
+    users.forEach(user => {
+      const userCard = userCardTemplate.cloneNode(true)
+      
+      userCard.querySelector('img').src = user.avatar
+      userCard.querySelector('h2').textContent = `${user.first_name} ${user.last_name}`
+      userCard.querySelector('p').textContent = `E-mail: ${user.email}`
+      userContainer.appendChild(userCard)
+    })
+    
+  } catch (error) {
+    console.error('Erro ao buscar usuários', error)
+    
+  }
+
+}
